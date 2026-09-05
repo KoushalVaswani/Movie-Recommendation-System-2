39400ba234117405e728b3abda55d3d48063efac
KoushalVaswani <koushalvaswani3@gmail.com>
2026-09-05 22:58:47 +0530

    Point frontend to live Render backend

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://movie-recommendation-system-2-2.onrender.com"
 
 st.set_page_config(
     page_title="Movie Recommender",
